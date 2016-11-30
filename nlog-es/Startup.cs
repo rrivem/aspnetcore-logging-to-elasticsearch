289931040c95878d7b1b1e741e528ea0332725a4
Rodrigo <rrivem@gmail.com>
2016-11-30 16:33:22 -0300

    fix NLog logs files location for unix based systems

--- nlog-es/Startup.cs
+++ nlog-es/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog;
+using NLog.Config;
 using NLog.Extensions.Logging;
 using NLog.Targets;
 using WebApplication.Data;
@@ -62,6 +63,8 @@ namespace WebApplication
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            LogManager.Configuration = new XmlLoggingConfiguration("nlog.config");
 
             loggerFactory.AddNLog();
 
